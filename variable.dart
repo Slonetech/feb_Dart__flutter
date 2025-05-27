9567e7a0344b5ad23261fe16dc3eee8c107170e5
Slonetech <kengeregordon@gmail.com>
2025-05-27 11:40:55 +0300

    Add initial Dart files for Hello World and variable demonstration

---
+++ variable.dart
@@ -0,0 +1,10 @@
+// variable in dart
+void main() {
+  var name = "Linus";
+  var age = 20;
+  var country = "Kenya";
+  final ID = 39862227;
+  const PI = 3.14;
+
+  print ("my is $name and i am $age ");
+}
